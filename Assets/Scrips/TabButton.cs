838899c07165940b117bd802b4b69905a4da871d
HobieDog <57800848+HobieDog@users.noreply.github.com>
2019-11-24 16:29:43 +0900

    Initial commit

---
+++ Assets/Scrips/TabButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabButton : MonoBehaviour
+{
+    Image background;
+    public Sprite idelImg;
+    public Sprite selectedImg;
+
+    private void Awake()
+    {
+        background = GetComponent<Image>();
+    }
+
+    public void Selected()
+    {
+        background.sprite = selectedImg;
+    }
+
+    public void DeSelected()
+    {
+        background.sprite = idelImg;
+    }
+}
